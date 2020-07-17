67b9c0d8f1ddbd2975895c9c796c1c47c04f98cd
gfenelus <gfenelus@student.neumont.edu>
2020-07-16 20:38:31 -0600

    made a connection to mongodb
    used mongodb driver version 3.6.3, crud HAS NOT been implemented yet

--- src/prog/Main.java
+++ src/prog/Main.java
@@ -1,8 +1,42 @@
 package prog;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ServerAddress;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import java.util.Arrays;
+import com.mongodb.Block;
+
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import org.bson.io.BsonOutput;
+
+import java.util.ArrayList;
+import java.util.List;;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        connectDB();
+
     }
+    public static void connectDB(){
+        MongoClient mongo = new MongoClient( "localhost" , 27017 );
+        MongoDatabase test = mongo.getDatabase("test");
+        MongoCollection<Document> people = test.getCollection("people");
+
+        System.out.println(people.find().first());
+
+        //TODO make database and collection class variables before implementing CRUD methods
+    }
+
+
+
 }
